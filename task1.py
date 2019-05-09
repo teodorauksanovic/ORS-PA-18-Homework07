b7b5b086622abe6b97d57b7ef6e4f36d134a9db3
lenovo <teodora.uksanovic@udg.edu.me>
2019-05-09 22:47:29 +0200

    .

--- task1.py
+++ task1.py
@@ -17,3 +17,29 @@
 """
 
 # Write your script here
+
+import random
+def rollthedice():
+    n = int(input("Number of times the dice should be rolled: "))
+    a1 = a2 = a3 = a4 = a5 = a6 = 0
+    for i in range(n):
+        number = random.randrange(1, 7)
+        if number == 1:
+            a1 += 1
+        if number == 2:
+            a2 += 1
+        if number == 3:
+            a3 += 1
+        if number == 4:
+            a4 += 1
+        if number == 5:
+            a5 += 1
+        if number == 6:
+            a6 += 1
+
+
+
+    print("Times number 1 appeared :", a1, "number 2: ", a2, "number 3:", a3, "number 4:", a4, "number 5:", a5,
+                  "number 6:", a6)
+
+rollthedice()
